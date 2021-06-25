1482f057e9dbe95661fc88bd060fc93b793f6480
Hassan Farahani <farahani.h1360@gmail.com>
2021-06-25 16:10:16 -0700

    add some styles

--- src/components/GameInfoDetails.vue
+++ src/components/GameInfoDetails.vue
@@ -1,11 +1,14 @@
 <template>
+    <div>
+        <h1 class="game-header">Game Details</h1>
+    </div>
     <div class="container grid-container game-details">
         <div class="game-header-details">Game Name</div>
         <div class="game-header-details">Game Description</div>
         <div class="game-header-details">Game Players Usernames</div>
-        <div class=""> {{ gameDetails ? gameDetails.name : null }}</div>
-        <div class="">{{ gameDetails ? gameDetails.description : null }}</div>
-        <div class="">
+        <div class="game-detail"> {{ gameDetails ? gameDetails.name : null }}</div>
+        <div class="game-detail">{{ gameDetails ? gameDetails.description : null }}</div>
+        <div class="game-detail">
             <span v-for="player in gamePlayers" :key="player.accountid">{{ player.username }} - </span>
         </div>
     </div>
@@ -22,11 +25,23 @@ export default {
 .grid-container {
     display: grid;
     grid-template-columns: 1fr 2fr 2fr;
-    grid-gap: 1rem;
+    grid-gap: 2rem;
 }
 
 .game-header-details {
     font-weight: bold;
     font-size: 1.5rem;
+}
+
+.game-details {
+    background: #afe3bd;
+}
+
+.game-detail {
+    font-style: italic;
+}
+
+.game-header {
+    text-align: center;
 }
 </style>
